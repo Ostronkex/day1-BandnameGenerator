4b737ccc50c9dbf9cf3b4d2e636f00ae1e499225
Ostronkex <erikmagnusson.privat@gmail.com>
2024-07-12 13:18:56 +0200

    Add files via upload

---
+++ day1-BandnameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator") #Printar ett välkomstmeddelande
+city = input("What is the name of the city you grew up in?") #Variabel som heter city. Input frågar anv vilken stad de växte upp i, tilldelar sedan detta värde/stad till city
+pet = input("What is the name of your pet?") #Samma som ovan, variabel pet, frågar efter namn på djuret och tilldelar värdet till variabeln
+
+bandName = city + ' ' + pet #Variabel bandName. Tilldelar värdet av city + mellanrum + namn på djuret
+print(bandName) #Printar värdet av bandName
